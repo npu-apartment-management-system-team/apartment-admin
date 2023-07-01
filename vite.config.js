c0ec1aa92089858ad316467d8bb1e534f16296e2
WangMinan <wangminan0811@hotmail.com>
2023-07-01 11:29:51 +0800

    完善部署与打包脚本

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import topLevelAwait from 'vite-plugin-top-level-await'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: './',
+    publicDir: 'public',
+    plugins: [react(),
+        // 启用顶级js文件中的await
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: '__tla',
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: i => `__tla_${i}`
+        })
+    ],
+    build: {
+        // 构建打包时规范js文件大小
+        chunkSizeWarningLimit: 500,
+        rollupOptions: {
+            output: {
+                // eslint-disable-next-line consistent-return
+                manualChunks (id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
